7f466faff00758b6ad3797de4ca807c81d27561f
skwak <skwak@425cac29-081b-6f46-9e87-dc0dc517dd96>
2014-06-30 09:38:33 +0000

    연습 객체 삽입수를 늘림

--- stream/main.cpp
+++ stream/main.cpp
@@ -12,7 +12,7 @@ int main(int argc, char *argv[])
 	if(ts.open(0)<0)
 		ACE_ERROR_RETURN ((LM_ERROR,ACE_TEXT ("%p\n"),ACE_TEXT ("CTestStream.open()")),0);
 
-	for(int i=0;i<2;i++) {
+	for(int i=0;i<10;i++) {
 		sprintf(buffer,"message %d\n",i+1);
 		ts.insert(buffer,(int)strlen(buffer)+1);	//NULLÆ÷ÇÔ
 	}

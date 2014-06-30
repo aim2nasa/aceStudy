7a938f7733ea53fd9a0529b2c7efd1bdc33304e6
skwak <skwak@425cac29-081b-6f46-9e87-dc0dc517dd96>
2014-06-30 08:47:21 +0000

    어느 스레드에서 메세지를 처리하는 지를 표시하도록 수정

--- stream/CEnd.h
+++ stream/CEnd.h
@@ -2,12 +2,16 @@
 #define __CEND_H__
 
 #include "CBasicTask.h"
+#include <assert.h>
 
 class CEnd : public CBasicTask {
 protected:
 	virtual int process (ACE_Message_Block *mb)
 	{
-		ACE_DEBUG ((LM_DEBUG,ACE_TEXT ("CEnd::process()\n")));
+		const char *cp = mb->rd_ptr();
+		assert(cp);
+
+		printf("CEnd::process : %s",cp);
 		return 0;
 	}
 };

216d31c01100b565ece15953c82117b786a6c196
Rossi Kwak <26228531+aim2nasa@users.noreply.github.com>
2015-05-14 05:46:56 +0000

    \n 문자열 잘못 들어간 버그 수정

--- reactorCli/client.cpp
+++ reactorCli/client.cpp
@@ -44,7 +44,7 @@ int main(int argc, char *argv[])
 		if ((nRtn=client_stream.recv_n(recv_buff, sizeof(recv_buff))) == -1)
 			ACE_ERROR((LM_ERROR, "(%P|%t) Error recv_n(%d)\n",nRtn));
 		else
-			ACE_DEBUG((LM_DEBUG, "(%P|%t) %dbytes received:%sn", nRtn,recv_buff));
+			ACE_DEBUG((LM_DEBUG, "(%P|%t) %dbytes received:%s\n", nRtn,recv_buff));
 	}
 
 	ACE_OS::sleep(1);

884306ce9455ea538e74e5d641f9e5f79535f9a2
skwak <skwak@425cac29-081b-6f46-9e87-dc0dc517dd96>
2015-05-14 01:35:10 +0000

    Client 클래스를 삭제하고 메인에서 모든 내용을 구현

--- reactorCli/client.cpp
+++ reactorCli/client.cpp
@@ -8,46 +8,20 @@
 
 #define SIZE_BUF 256
 
-class Client
-{
-public:
-	Client(char *hostname, int port) :remote_addr_(port, hostname)
-	{
-	}
-
-	int connect_to_server();
-
-	ACE_SOCK_Stream client_stream_;
-	ACE_INET_Addr remote_addr_;
-	ACE_SOCK_Connector connector_;
-};
-
-int
-Client::connect_to_server()
-{
-	ACE_DEBUG((LM_DEBUG, "(%P|%t) Starting connect to %s: %d \n",
-		remote_addr_.get_host_name(), remote_addr_.get_port_number()));
-
-	if (connector_.connect(client_stream_, remote_addr_) == -1)
-	{
-		ACE_ERROR_RETURN((LM_ERROR, "(%P|%t) %p \n", "connection failed"), -1);
-	}
-	else
-	{
-		ACE_DEBUG((LM_DEBUG, "(%P|%t) connected to %s \n",
-			remote_addr_.get_host_name()));
-	}
-
-	return 0;
-}
-
 static char* SERVER_HOST = "127.0.0.1";
 static u_short SERVER_PORT = 19001;
 
 int main(int argc, char *argv[])
 {
-	Client client(SERVER_HOST, SERVER_PORT);
-	client.connect_to_server();
+	ACE_SOCK_Stream client_stream;
+	ACE_INET_Addr remote_addr(SERVER_PORT,SERVER_HOST);
+	ACE_SOCK_Connector connector;
+
+	ACE_DEBUG((LM_DEBUG, "(%P|%t) Starting connect to %s: %d \n",remote_addr.get_host_name(), remote_addr.get_port_number()));
+	if (connector.connect(client_stream, remote_addr) == -1)
+		ACE_ERROR_RETURN((LM_ERROR, "(%P|%t) %p \n", "connection failed"), -1);
+	else
+		ACE_DEBUG((LM_DEBUG, "(%P|%t) connected to %s \n",remote_addr.get_host_name()));
 
 	char buffer[SIZE_BUF];
 	while (true){
@@ -55,14 +29,14 @@ int main(int argc, char *argv[])
 		std::cin >> buffer;
 
 		int n = 0;
-		n = client.client_stream_.send_n(buffer, SIZE_BUF);
+		n = client_stream.send_n(buffer, SIZE_BUF);
 		if (n == -1) {
 			ACE_ERROR_RETURN((LM_ERROR, "(%P|%t) %p \n", "send_n"), 0);
 		}
 
 		// recv
 		char recv_buff[SIZE_BUF] = { 0 };
-		n = client.client_stream_.recv_n(recv_buff, sizeof(recv_buff));
+		n = client_stream.recv_n(recv_buff, sizeof(recv_buff));
 		if (n == -1) {
 			ACE_ERROR((LM_ERROR, "%p \n", "Error in recv"));
 		}
@@ -73,7 +47,7 @@ int main(int argc, char *argv[])
 
 	ACE_OS::sleep(1);
 
-	if (client.client_stream_.close() == -1)
+	if (client_stream.close() == -1)
 		ACE_ERROR_RETURN((LM_ERROR, "(%P|%t) %p \n", "close"), -1);
 
 	return 0;
